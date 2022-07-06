a626361687cd282bb4dcc796b538ba84d3b8d3d1
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-07-06 19:07:02 -0300

    add body style

---
+++ my-app/src/components/StyledBody/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledBody = styled.body`
+  margin-left: 45rem;
+  margin-right: 45rem;
+  font-family: "Poppins";
+`;
+
+export const StyledDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+`;
