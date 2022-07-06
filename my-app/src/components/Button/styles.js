fb5dec1260eb0eaa70a79cc7a3d33008408a8490
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-07-06 19:06:31 -0300

    add button styling

---
+++ my-app/src/components/Button/styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 2rem;
+  margin-top: 1rem;
+`;
+export const StyledButton = styled.button`
+  background-color: #457b9d;
+  color: white;
+  border: none;
+  padding: 0.5rem 2rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  font-family: "Poppins";
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  width: 30%;
+  border-radius: 0.5rem;
+`;
+export const StyledButton1 = styled.button`
+  background-color: #457b9d;
+  color: white;
+  border: none;
+  padding: 0.5rem 2rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  font-family: "Poppins";
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  width: 30%;
+  border-radius: 0.5rem;
+`;
