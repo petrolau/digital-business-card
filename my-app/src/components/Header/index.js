bd09ec1afeff2bc8362dafe0e8faf5a4f58f9e7e
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-07-06 19:07:31 -0300

    add Header component

---
+++ my-app/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import { StyledDiv, StyledH1, StyledSpan, StyledSpan1 } from "./styles";
+const Header = () => {
+  return (
+    <StyledDiv>
+      <StyledH1>Kim Jennie</StyledH1>
+      <StyledSpan1>Frontend Developer</StyledSpan1>
+      <StyledSpan>kimjennie.website</StyledSpan>
+    </StyledDiv>
+  );
+};
+
+export default Header;
---
+++ my-app/src/components/Header/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledH1 = styled.h1`
+  color: #282d35;
+  font-weight: 800;
+  padding: 0;
+  margin: 0;
+  margin-top: 1rem;
+`;
+
+export const StyledSpan = styled.span`
+  color: #282d35;
+  font-weight: 500;
+  padding: 0.5rem;
+`;
+export const StyledSpan1 = styled.span`
+  color: #457b9d;
+  font-weight: 500;
+  padding: 0.5rem;
+`;
