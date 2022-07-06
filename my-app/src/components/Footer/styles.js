65a30ccd881a73bf1da22dbed2b3252b7606c4fc
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-07-06 19:06:48 -0300

    add footer styling

---
+++ my-app/src/components/Footer/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+export const StyledFooter = styled.footer`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  background-color: #457b9d;
+  padding: 1rem;
+  font-size: 40px;
+  color: white;
+`;
+
+export const StyledDiv = styled.div``;
