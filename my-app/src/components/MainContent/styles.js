a1a9eb4f799e08cb8e14ded578e017ad78305c6b
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-07-06 19:07:13 -0300

    add mainContent component

---
+++ my-app/src/components/MainContent/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1rem;
+  margin-left: 1rem;
+  margin-bottom: 1rem;
+`;
+
+export const StyledH2 = styled.h2`
+  color: #282d35;
+`;
+
+export const StyledSpan = styled.span`
+  color: #282d35;
+`;
