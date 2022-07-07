60587e3c0f7910e92cc48f62a8d17d55c5107c50
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-07-07 19:56:26 -0300

    add media queries

--- my-app/src/components/ImageCard/styles.js
+++ my-app/src/components/ImageCard/styles.js
@@ -2,6 +2,12 @@ import styled from "styled-components";
 
 export const StyledImg = styled.img`
   width: 29rem;
+  @media only screen and (max-width: 800px) {
+    width: 25.8rem;
+  }
+  @media only screen and (max-width: 600px) {
+    width: 31rem;
+  }
 `;
 
 export const StyledDiv = styled.div`
--- my-app/src/components/StyledBody/styles.js
+++ my-app/src/components/StyledBody/styles.js
@@ -4,6 +4,14 @@ export const StyledBody = styled.body`
   margin-left: 45rem;
   margin-right: 45rem;
   font-family: "Poppins";
+  @media only screen and (max-width: 800px) {
+    margin-left: 10rem;
+    margin-right: 10rem;
+  }
+  @media only screen and (max-width: 600px) {
+    margin-left: 0;
+    margin-right: 0;
+  }
 `;
 
 export const StyledDiv = styled.div`
